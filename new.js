f8801c42ca730b10daec97a6122099dec0f7a04c
edwinaringo <tedaringo628@gmail.com>
2021-03-01 17:38:21 +0300

    Re-arranging th eportfolio section

--- new.js
+++ new.js
@@ -1,11 +1,13 @@
 //  the what we do section
 $(document). ready(function(){
     $('.icon1').click(function(){
-      $('p1').toggle();
+        $('p1').toogle();
     });
+
     $('.icon2').click(function(){
       $('p2').toggle();
     });
+
     $('.icon3').click(function(){
       $('p3').toggle();
     });
